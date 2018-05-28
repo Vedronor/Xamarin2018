5a15439754aa8648b3d5c1839a3b9b85744ba893
Vitor l <amilrotiv@hotmail.com>
2018-05-28 16:35:57 -0300

    App01 - Conluido

---
+++ Seção05/App01_consultarCEP/App01_consultarCEP/App01_consultarCEP/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_consultarCEP.Servico.Modelo;
+using App01_consultarCEP.Servico;
+namespace App01_consultarCEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+		}
+
+        private void BuscarCEP(object sender, EventArgs args) {
+            //TUDO - Lógica do Programa.
+
+            //TUDO - Validações.
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço: {0} {1}, {3}, {2}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                    else {
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP informado: " + cep, "OK");
+                    }
+                    }
+                catch (Exception e) {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
+                }
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+            if (cep.Length != 8 ) {
+                DisplayAlert("ERRO", "CEP inválido. O CEP deve conter 8 caracteres.", "Ok");
+
+                valido = false;
+            }
+            int novoCEP = 0;
+            if (!int.TryParse(cep, out novoCEP)) {
+                DisplayAlert("ERRO", "CEP inválido. O CEP deve ser composto apenas por números.", "OK");
+                valido = false;
+            }
+            
+            return valido;
+        }
+
+    }
+}
